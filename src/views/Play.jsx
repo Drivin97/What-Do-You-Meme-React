26cb35ce2e6f509d4589540679a0b8e99b2cae78
Dwayne <driv91197@gmail.com>
2018-11-29 12:09:11 -0500

    Added features to React App

--- src/views/Play.jsx
+++ src/views/Play.jsx
@@ -1,12 +1,45 @@
-import React from "react";
+import React, { Component} from "react";
+import * as api from "../services/api_access";
 
-export default () => (
-    <div className="card">
-        <div className="card-header">
-            Play Game
+export default class App extends Component {
+
+    state = {
+        players:[
+            {id: 123, name: "Dwayne", fbid: 807, score: 33}
+        ]
+    }
+
+    componentDidMount() {
+        const loopTimer = setInterval(this.refresh, 1000);
+    }
+
+    refresh = () => {
+        api.GetState().then(x => this.setState(x))
+    }
+
+    render() {
+        const players = this.state.players.map(p => (
+            <li key={p.id}
+                class="list-group-item">
+                <img src={`https://graph.facebook.com/${p.fbid}/picture`} />
+                <h5>{p.name}</h5>
+                <span class="badge badge-success">
+                    Dealer
+                </span> &nbsp;
+                <span class="badge badge-primary badge-pill">{p.score}</span>
+            </li>
+        ))
+      return (
+        <div className="card">
+            <div className="card-header">
+                Play Game
+            </div>
+            <div className="card-body">
+                Here are the players
+            </div>
+            <ul class="list-group list-group-flush">
+                {players}
+            </ul>              
         </div>
-        <div className="card-body">
-            Here are the players
-        </div>
-    </div>
-)
+    )}
+}
